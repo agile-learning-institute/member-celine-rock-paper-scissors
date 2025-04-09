17e35124747e8a981906f2f7a4e2bc2361288b7c
celinerobins <celinerobinson123e@gmail.com>
2025-04-09 19:01:59 -0400

    Write logic to get human choice

--- rps.js
+++ rps.js
@@ -1,3 +1,15 @@
+function getHumanChoice() {
+    let humanChoice = prompt('Input "Rock", "Paper", or "Scissors"!');
+    if (humanChoice === "Rock") {
+        return "Rock";
+    } else if (humanChoice === "Paper") {
+        return "Paper";
+    } else if (humanChoice === "Scissors"){
+        return "Scissors"; 
+    }
+}
+console.log(getHumanChoice());
+
 function getComputerChoice () {
     var compChoice = Math.random() * 3;
     if (compChoice <= 1) {
