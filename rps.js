ba96b891e8bcc26ec94dd279f69afd6357337cf3
celinerobins <celinerobinson123e@gmail.com>
2025-04-09 18:49:55 -0400

    Write logic to get computer choice

--- rps.js
+++ rps.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+function getComputerChoice () {
+    var compChoice = Math.random() * 3;
+    if (compChoice <= 1) {
+        return "Rock";
+    } else if (compChoice <= 2) {
+        return "Paper";
+    } else {
+        return "Scissors"; 
+    }
+}
+
+console.log(getComputerChoice());
